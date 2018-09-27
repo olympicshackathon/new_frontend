994ba9324c5236f3052a41a9d1d7fbbf5ae4540b
Brian Bixby <brianbixby0@gmail.com>
2018-09-27 16:47:22 -0700

    pushing

--- src/actions/map-actions.js
+++ src/actions/map-actions.js
@@ -11,11 +11,13 @@ export const currentLocationFetch = location => ({
 });
 
 export const googlePlacesFetchRequest = location => dispatch => {
-  return superagent.get(`https://maps.googleapis.com/maps/api/js?key=${process.env.__GAPI_KEY__}&libraries=places`)
+  console.log('location argument: ', location);
+  // return superagent.get(`https://maps.googleapis.com/maps/api/js?key=${process.env.__GAPI_KEY__}&libraries=places`)
+  return superagent.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location.lat},${location.lng}&radius=1600&type=restaurant&key=${process.env.__GAPI_KEY__}&libraries=places`)
     .then(res => {
-        console.log('res: ', res);
+        console.log('res: ', res.body.results);
       dispatch(googlePlacesFetch(res.body.results));
-      return res;
+      return res.body.results;
     });
 };
 
@@ -32,5 +34,17 @@ export const currentLocationFetchRequest = () => dispatch => {
     dispatch(currentLocationFetch({ lat: 48.8566, lng: 2.3522 }));
   }
 };
+
+// export const locationPhotoFetchRequest = photoRef => dispatch => {
+//   console.log('photoRef argument: ', photoRef);
+//   // return superagent.get(`https://maps.googleapis.com/maps/api/js?key=${process.env.__GAPI_KEY__}&libraries=places`)
+//   return superagent.get(`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoRef}&key=${process.env.__GAPI_KEY__}`)
+//     .then(res => {
+//         console.log('res: ', res.body.results);
+//       dispatch(googlePlacesFetch(res.body.results));
+//       return res.body.results;
+//     });
+// };
+
 
 // `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location.lat},${location.lng}&radius=1600&type=restaurant&key=${process.env.__GAPI_KEY__}`
--- src/components/map-container/index.js
+++ src/components/map-container/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { browserHistory } from 'react-router';
+import ReactDOM from 'react-dom';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import PropTypes from 'prop-types';
+import superagent from 'superagent';
 
 import { tokenSignInRequest } from '../../actions/userAuth-actions.js';
 import { userProfileFetchRequest, userProfileUpdateRequest } from '../../actions/userProfile-actions.js';
-import { googlePlacesFetchRequest } from '../../actions/map-actions.js';
+import { googlePlacesFetchRequest, currentLocationFetchRequest } from '../../actions/map-actions.js';
 import Modal from '../helpers/modal';
 import { userValidation, logError, renderIf } from './../../lib/util.js';
 
@@ -25,40 +27,29 @@ class MapContainer extends React.Component {
     componentWillMount() {
         userValidation(this.props);
     }
-    // https://hackernoon.com/replacing-componentwillreceiveprops-with-getderivedstatefromprops-c3956f7ce607
-    // componentWillReceiveProps(nextProps){
-    //     if(nextProps.currentLocation !== this.props.currentLocation){
-    //       let {firebaseRef}=this.state;
-          
-    //       firebaseRef.off("value"); //Turn off the connection to previous path.
-          
-    //       firebaseRef=firebase.database().ref(nextProps.path);
-    //       this.setState({firebaseRef, path :nextProps.path });
-    //       this.getData(firebaseRef);
-    //     }
-    // }
-    componentDidMount() {
-        // this.props.currentLocationFetch();
-        this.mounted = true;
 
-        if (this.mounted)
-            this.updatePosition();
-    }
     componentWillUnmount(){
         this.mounted = false;
     };
 
-    // handleGooglePlacesFetch = location => {
-    //     console.log('location: ', location);
-    //     return this.props.googlePlacesFetch(location)
-    //         .then(results => console.log(results))
-    //         .catch(logError);
-    // };
+    componentDidMount() {
+        this.mounted = true;
 
-    updatePosition = () => {
-        // this.handleGooglePlacesFetch({ lat: pos.coords.latitude, lng: pos.coords.longitude });
-        console.log('update position func');
+        if (this.mounted)
+        {
+            this.setState({ currentLocation: this.props.currentLocation });
+            return this.props.googlePlacesFetch(this.props.currentLocation)
+                .then(results => this.searchNearbyCB(results))
+                .catch(logError);
+        }
     }
+
+    // componentDidUpdate(prevProps, prevState) {
+    //     if (prevProps.google !== this.props.google) {
+
+    //     }
+
+    // }
 
     onMapReady = (mapProps, map) => {
         this.searchNearby(map, map.center);
@@ -74,8 +65,6 @@ class MapContainer extends React.Component {
     searchNearby = (map, center) => {
         const { google } = this.props;
         const service = new google.maps.places.PlacesService(map);
-    
-        // Specify location, radius and place types for your Places API search.
         const request = {
           location: center,
           radius: '5000',
@@ -84,18 +73,9 @@ class MapContainer extends React.Component {
     
         service.nearbySearch(request, this.searchNearbyCallback);
         this.setState({ map: map });
-        console.log('map state: ', this.state.map);
-        // service.nearbySearch(request, (results, status) => {
-        //     if (status === google.maps.places.PlacesServiceStatus.OK)
-        //     {
-        //       console.log('results: ', results);
-        //       this.setState({ places: results });
-        //     }
-        //   });
     };
 
     searchNearbyCallback = (results, status, pagination) => {
-        console.log('marketscallback()');
         if (status === google.maps.places.PlacesServiceStatus.OK) {
             for (var i = 0; i < results.length; i++) {
 
@@ -107,7 +87,8 @@ class MapContainer extends React.Component {
                     name: results[i].name,
                     photos: []
                 };
-                results[i].photos.forEach(pho => place.photos.push(pho.getUrl({'maxWidth': 600, 'maxHeight': 400})))
+                if (results[i].photos)
+                    results[i].photos.forEach(pho => place.photos.push(pho));
                 this.setState({ places: place });
                 this.createMarker(place);
             }
@@ -117,8 +98,52 @@ class MapContainer extends React.Component {
         }
     };
 
+    searchNearbyCB = results => {
+        console.log('searchNearbyCB: ', results);
+        for (var i = 0; (i < results.length && i < 10); i++) {
+            let place = {
+                location: {
+                    lat: results[i].geometry.location.lat,
+                    lng: results[i].geometry.location.lng
+                },
+                name: results[i].name,
+                photos: []
+            };
+            if (results[i].photos)
+                results[i].photos.forEach(pho => place.photos.push(pho))
+            this.setState({ places: place });
+            // this.renderMarker(place);
+            this.createMarker(place);
+        }
+        console.log('state.places: ', this.state.places);
+    };
+
+    renderMarker = (item) => {
+        this._map.setZoom(1);
+        let {
+          map, google, position, mapCenter
+        } = this.props;
+  
+        let pos = item.location || mapCenter;
+        position = new google.maps.LatLng(item.location.lat, item.location.lng);
+  
+        const pref = {
+          map: map,
+          position: position
+        };
+        this.marker = new google.maps.Marker(pref);
+    }
+
+    // locationPhotoFetchRequest = photoRef =>  {
+    //     console.log('photoRef argument: ', photoRef);
+    //     return superagent.get(`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoRef}&key=${process.env.__GAPI_KEY__}`)
+    //         .then(res => {
+    //             console.log('location photo reg res: ', res.body.results);
+    //             return res.body.results;
+    //         });
+    // };
+
     createMarker = item => {
-        console.log('createmarker()');
         const { google } = this.props;
         var marker = new google.maps.Marker({
             map: this.state.map,
@@ -170,16 +195,21 @@ class MapContainer extends React.Component {
     render() {
         const style = { width: '100%', height: '100%' };
         return (
-            <Map google={this.props.google} zoom={14} center={this.props.currentLocation} 
-                 onReady={this.onMapReady} onClick={this.onMapClicked} >
+            
+
+            <Map google={this.props.google} zoom={14} center={this.state.currentLocation} 
+                //  onReady={this.onMapReady}
+                 onClick={this.onMapClicked} ref={(map) => this._map = map}>
+                 {/* <div ref='map' className='mapInnerDiv' /> */}
+                 
                 <Marker onClick={this.onMarkerClick}
                         onMouseover={this.onMouseoverMarker}
                         name={'Current location'} 
                         position={this.props.currentLocation}
                         icon={{
                             url: 'https://i.imgur.com/Oa8iJO5.png',
-                            anchor: new google.maps.Point(32, 32),
-                            scaledSize: new google.maps.Size(64, 64)
+                            anchor: new google.maps.Point(8, 8),
+                            scaledSize: new google.maps.Size(16, 16)
                         }}
                         />
                 <InfoWindow onClose={this.onInfoWindowClose} marker={this.state.activeMarker}
@@ -195,12 +225,14 @@ let mapStateToProps = state => ({
     userAuth: state.userAuth,
     userProfile: state.userProfile,
     googlePlaces: state.googlePlaces,
+    currentLocation: state.currentLocation,
 });
   
 let mapDispatchToProps = dispatch => ({
     tokenSignIn: token => dispatch(tokenSignInRequest(token)),
     userProfileFetch: () => dispatch(userProfileFetchRequest()),
     userProfileUpdate: profile => dispatch(userProfileUpdateRequest(profile)),
+    currentLocationFetch: () => dispatch(currentLocationFetchRequest()),
     googlePlacesFetch: location => dispatch(googlePlacesFetchRequest(location)),
 });
 
